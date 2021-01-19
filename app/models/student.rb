5e543024dd51c96215605d319d708f7a8e5e9f62
yanyan7 <sakuraperfection@gmail.com>
2021-01-19 14:18:24 +0900

    アソシエーション実装完了

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,4 @@
 class Student < ApplicationRecord
-  has_many :subjects, through: :student_subjects
   has_many :student_subjects
-
+  has_many :subjects, through: :student_subjects
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,4 @@
 class Subject < ApplicationRecord
-  has_many :students, through: :student_subjects
   has_many :student_subjects
+  has_many :students, through: :student_subjects
 end
